dfe3faad5d9ab7a55d4b29671652c0b4d0fd15de
Alex Bucknall <alex.bucknall@gmail.com>
2020-04-22 14:08:37 +0100

    [patch] for linux-xlnx-v2018.3-zynqmp-fpga.

--- Makefile
+++ Makefile
@@ -5,12 +5,12 @@ CORE=fan_control
 TARGET=ultra96_v1
 FLAGS=linux
 
-GIT_VER=$(shell git describe --tags)
+GIT_VER=$(shell git describe --tags --abbrev=0)
 
 .PHONY: all build flash clean
 
 all:
-	fusesoc --verbose run --tool=vivado --target=$(TARGET) --flag=$(FLAGS) $(USER):$(LIB):$(CORE):$(GIT_VER) 
+	fusesoc run --tool=vivado --target=$(TARGET) --flag=$(FLAGS) $(USER):$(LIB):$(CORE):$(GIT_VER) 
 
 build:
 	fusesoc run --build --tool=vivado --target=$(TARGET) --flag=$(FLAGS) $(USER):$(LIB):$(CORE):$(GIT_VER)
--- sw/export_hardware.tcl
+++ sw/export_hardware.tcl
@@ -3,7 +3,7 @@ open_project [glob *.xpr]
 set project_name [current_project]
 
 if {[info exists sdk_workspace] == 0} {
-    set sdk_workspace ${project_name}.sdk
+    set sdk_workspace ${project_name}.linux
 }
 if { [file exists ${sdk_workspace}] == 0 } {
     file mkdir ${sdk_workspace}
@@ -11,5 +11,8 @@ if { [file exists ${sdk_workspace}] == 0 } {
 
 set design_top_name [get_property "top" [current_fileset]]
 file copy -force [file join $project_name.runs "impl_1" $design_top_name.hwdef] [file join $sdk_workspace $design_top_name.hwdef]
+file copy -force [file join $project_name.runs "impl_1" $design_top_name.sysdef] [file join $sdk_workspace $design_top_name.hdf]
+file copy -force [file join $project_name.runs "impl_1" $design_top_name.sysdef] [file join $sdk_workspace $design_top_name.sysdef]
+file copy -force [file join $project_name.runs "impl_1" $design_top_name.bit] [file join $sdk_workspace $design_top_name.bit]
 
 close_project
